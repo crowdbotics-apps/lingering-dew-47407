c56ec69a678e2320e3a499b8ad65479a5006ea77
Crowdbotics <team@crowdbotics.com>
2024-04-06 14:15:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lingering-dew-47407",
+  url: "https://lingering-dew-47407.botics.co",
+  api: "https://lingering-dew-47407.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
